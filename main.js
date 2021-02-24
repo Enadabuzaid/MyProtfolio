3a4b7f9feeeeaf3e6223e56814330676ce71a1b3
Enad Abuzaid <enad.abuzaid15@gmail.com>
2021-02-25 01:17:16 +0200

    night code

--- main.js
+++ main.js
@@ -4,10 +4,19 @@
       const aboutSection = document.querySelector(".about-section"),
       tabsContainer =document.querySelector(".about-tabs");
 
-      tabsContainer.addEventlisener("click",(event)=>{
+      tabsContainer.addEventListener("click",(event)=>{
       	if (event.target.classList.contains("tab-item") &&
-      		!event.target.classList.contais("active")) {
+      		!event.target.classList.contains("active")) {
       		const target=event.target.getAttribute("data-target");
+      		//deactivate existing active 'tab-item'
+      		tabsContainer.querySelector(".active").classList.remove("outer-shadow","active");
+      		//activate new  'tab-item'
+      		event.target.classList.add("active","outer-shadow");
+      		//deactivate existing active 'tab-content'
+			aboutSection.querySelector(".tab-content.active").classList.remove("active");
+      		//activate new  'tab-content'
+			aboutSection.querySelector(target).classList.add("active");
+
       	}
       })
 })();
