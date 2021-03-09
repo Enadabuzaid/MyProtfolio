7cae50c33b5222b75f6f75e9ef474e6a9d984eb3
Enad Abuzaid <enad.abuzaid15@gmail.com>
2021-03-09 21:42:30 +0200

    Hi add js

--- main.js
+++ main.js
@@ -21,6 +21,9 @@
       })
 })();
 
+function bodyCrollingToggle(){
+	document.body.classList.toggle("stop-scrolling");
+}
 /* protfolio */
 
 (()=>{
@@ -39,33 +42,110 @@
 	filterContainer.addEventListener("click",(event)=>{
 		if(event.target.classList.contains("filter-item") && 
 		!event.target.classList.contains("active")){
-			filterContainer.querySelector(".active").classList.remove
-			("outer-shadow","active");
+			filterContainer.querySelector(".active").classList.remove("outer-shadow","active");
 			event.target.classList.add("active","outer-shadow");
 			const target = event.target.getAttribute("data-target");
+			
 			portfolioItems.forEach((item) =>{
-				if(target === item.getAttribute("data-category") ||
-				target==='all'){
+				
+			if(target === item.getAttribute("data-category") || target ==='all'){
 					item.classList.remove("hide");
-					item.classList.add("show");
-				} else{
-					item.classList.remove("show");
-					item.classList.add("hide")
-				}
-			})
-		}
+			 		item.classList.add("show");
+			 	} else{
+			 		item.classList.remove("show");
+			 		item.classList.add("hide")
+			 	}
+			 })
+		
+		} 
 	})
 
-	portfolioItemContainer.addEventListener("click",(event)=>{
-		if (event.target.closest(".protfolio-item-inner")) {
-			const portfolioItem = event.target.closest(".portfolio-item-inner").parentElement;
-			itemIndex = Array.from(portfolioItem.parentElement.children).indexOf(portfolioItem);
-			secreenshots = portfolioItem[itemIndex].querySelector(".prtfolio-item-img img").getAttribute("data-screenshots");
-			console.log(screenshots);
+	portfolioItemContainer.addEventListener("click", (event)=>{
+		
+		//console.log(event.target.closest(".portfolio-item-inner"));
+		if (event.target.closest(".portfolio-item-inner")) {
+	 		const portfolioItem = event.target.closest(".portfolio-item-inner").
+			 parentElement;
+	 		itemIndex = Array.from(portfolioItem.parentElement.children).indexOf(portfolioItem);
+	 		screenshots = portfolioItems[itemIndex].querySelector(".portfolio-item-img img").getAttribute("data-screenshots");
+			screenshots= screenshots.split(",");
+			if(screenshots.length===1){
+				prevBtn.style.display="none";
+				nextBtn.style.display="none";
+
+			} else{
+				prevBtn.style.display="block";
+				nextBtn.style.display="block";
+
+			}
+			slideIndex= 0;
+			popupToggle();
+			popupSlideshow();
+			popupDetails();
+			//console.log(screenshots);
+	 	}
+	 })
+
+	 closeBtn.addEventListener("click",()=>{
+		 popupToggle();
+	 })
+
+	 function popupToggle(){
+		 popup.classList.toggle("open");
+		 bodyCrollingToggle();
+	 }
+
+	
+	 function popupSlideshow(){
+		 const imgSrc = screenshots[slideIndex];
+		 	const popupImg = popup.querySelector(".pp-img");
+			 popup.querySelector(".pp-loader").classList.add("active");
+			 	popupImg.src=imgSrc;
+				popupImg.onload = () =>{
+					popup.querySelector(".pp-loader").classList.remove("active");
+			}
+			popup.querySelector(".pp-counter").innerHTML = (slideIndex+1) + " of " +
+			screenshots.length;	 
 		}
-	})
 
+		nextBtn.addEventListener("click", ()=>{
+			if(slideIndex === screenshots.length-1){
+				slideIndex =0;
+			} else{
+				slideIndex++;
+			}
+			popupSlideshow();
+		})
 
+		prevBtn.addEventListener("click", ()=>{
+			if(slideIndex === 0){
+				slideIndex =screenshots.length-1;
+			} else{
+				slideIndex--;
+			}
+			popupSlideshow();
+		})
 
+		function popupDetails(){
+			
+		}
+		projectDetailsBtn.addEventListener("click",()=>{
+			popupDetailsToggle();
+		})
+		function popupDetailsToggle(){
+			if(projectDetailsContainer.classList.contains("active")){
+				projectDetailsBtn.querySelector("i").classList.remove("fa-minus");
+				projectDetailsBtn.querySelector("i").classList.add("fa-plus");
+				projectDetailsContainer.classList.remove("active");
+				projectDetailsContainer.style.maxHeight = 0 + "px";
+				
+			} else{
+				projectDetailsBtn.querySelector("i").classList.remove("fa-plus");
+				projectDetailsBtn.querySelector("i").classList.add("fa-minus");
+				projectDetailsContainer.classList.add("active");
+				projectDetailsContainer.style.maxHeight = projectDetailsContainer.scrollHeight + "px";
+				popup.scrollTo(0,projectDetailsContainer.offsetTop);	
+		}
+		}
 
 })();
